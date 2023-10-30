913e9a71c0fe4f1fce9c42c5e427e402c45e57c7
Giwon Baek <122437705+BbaekGiwon@users.noreply.github.com>
2023-10-30 16:04:55 +0900

    Update odd_parity_checker_tb.v

--- 07주차/odd_parity_checker_tb.v
+++ 07주차/odd_parity_checker_tb.v
@@ -2,7 +2,7 @@
 module odd_parity_checker_tb;
     reg a, b, c, d, p;
     wire pec;
-    odd_parity_checker DUT (.a(a), .b(b), .c(c), .d(d), .p(p) .pec(pec));
+    odd_parity_checker DUT (.a(a), .b(b), .c(c), .d(d), .p(p), .pec(pec));
     initial begin
         a = 0; b = 0; c = 0; d = 0; p = 0; #20;        a = 0; b = 1; c = 0; d = 0; p = 0; #20;
         a = 0; b = 0; c = 0; d = 0; p = 1; #20;        a = 0; b = 1; c = 0; d = 0; p = 1; #20;
